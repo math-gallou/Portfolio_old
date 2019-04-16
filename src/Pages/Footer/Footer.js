d3e723f2322365e9af4dfd7d9eaf9b5c8cb5ecc0
Mathoug <math.gallou@gmail.com>
2019-04-16 22:49:57 +0200

    MAJ date de MAJ

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -5,7 +5,7 @@ function BoxFooter(props){
     return (
         <footer>
             <div>
-                Copyright © 2019 Mathilde Gallou - Dernière mise à jour : 24 Mars 2019 - Pour me contacter : <a href="mailto:math.gallou@gmail.com?Subject=Demande%20de%20contact" target="_top">math.gallou@gmail.com</a>
+                Copyright © 2019 Mathilde Gallou - Dernière mise à jour : 16 Avril 2019 - Pour me contacter : <a href="mailto:math.gallou@gmail.com?Subject=Demande%20de%20contact" target="_top">math.gallou@gmail.com</a>
             </div>
         </footer>);
 }
